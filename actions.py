31b02ffd81bf18ebc30e537492f40d3749364b41
Varnit Balivada <var@Varnits-MacBook-Pro.local>
2024-06-25 18:21:37 -0700

    actions changes

--- actions.py
+++ actions.py
@@ -6,6 +6,7 @@ if TYPE_CHECKING:
     from engine import Engine
     from entity import Entity
 
+
 class Action:
     def perform(self, engine: Engine, entity: Entity) -> None:
         """Perform this action with the objects needed to determine its scope.
@@ -18,9 +19,11 @@ class Action:
         """
         raise NotImplementedError()
 
+
 class EscapeAction(Action):
     def perform(self, engine: Engine, entity: Entity) -> None:
         raise SystemExit()
+
 
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
@@ -29,13 +32,13 @@ class MovementAction(Action):
         self.dx = dx
         self.dy = dy
 
-        def perform(self, engine: Engine, entity: Entity) -> None:
-            dest_x = entity.x + self.dx
-            dest_y = entity.y + self.dy
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
 
-            if not engine.game_map.in_bounds(dest_x, dest_y):
-                return  # Destination is out of bounds.
-            if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
-                return  # Destination is blocked by a tile.
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
 
-            entity.move(self.dx, self.dy)
+        entity.move(self.dx, self.dy)
